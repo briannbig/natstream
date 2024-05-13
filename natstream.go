702bf585602d263bd52471d837eb504c2b48359c
Brian <briannbig@gmail.com>
2024-05-13 17:55:56 +0300

    initial commit with basic consumer

---
+++ natstream.go
@@ -0,0 +1,56 @@
+package natstream
+
+import (
+	"context"
+	"log"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+)
+
+type (
+	consumer struct {
+		con      jetstream.Consumer
+		callback func(jetstream.Msg)
+	}
+
+	Queue struct {
+		stream jetstream.Stream
+	}
+)
+
+func New(ctx context.Context, nc *nats.Conn, streamName string, subjects []string) Queue {
+	js, err := jetstream.New(nc)
+	if err != nil {
+		log.Fatalf("Error connecting to jetstream --- %s", err.Error())
+	}
+
+	s, err := js.CreateOrUpdateStream(ctx, jetstream.StreamConfig{
+		Name:     streamName,
+		Subjects: subjects,
+		Storage:  jetstream.MemoryStorage,
+	})
+	if err != nil {
+		log.Fatalf("error creating stream --- %s", err.Error())
+	}
+
+	return Queue{stream: s}
+}
+
+func (q Queue) RegisterConsumer(ctx context.Context, handler func(jetstream.Msg), consumerDurableName string) error {
+
+	cons, _ := q.stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+		Durable:   consumerDurableName,
+		AckPolicy: jetstream.AckExplicitPolicy,
+	})
+	return newConsumer(cons, handler)
+}
+
+func newConsumer(con jetstream.Consumer, handler func(jetstream.Msg)) error {
+	var c = &consumer{
+		con:      con,
+		callback: handler,
+	}
+	_, err := c.con.Consume(c.callback)
+	return err
+}
